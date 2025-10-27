3ff35317e5c7f4b69f36af38c492ba18a2ffbc33
Kobaya65 <kobaya65@free.fr>
2025-10-27 12:52:40 +0100

    Refactored following SonarQube advices

--- primes.py
+++ primes.py
@@ -1,5 +1,10 @@
 """This module to hold is_prime() function.
 """
-def is_prime(num):
-    if num == 1:
-        return False
+def is_prime(num: int) -> bool:
+    """Checks whether num is prime number or not."""
+    if num < 2:
+        res = False
+    else:
+        res = True
+
+    return res
