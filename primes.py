c3726e20d0ecedc338e88bf0f8f5e04c04cfdc04
Kobaya65 <kobaya65@free.fr>
2025-10-27 18:10:01 +0100

    Improved is_prime()

--- primes.py
+++ primes.py
@@ -1,10 +1,25 @@
 """This module to hold is_prime() function.
 """
+from math import floor, sqrt
+
 def is_prime(num: int) -> bool:
-    """Checks whether num is prime number or not."""
+    """Checks whether num is prime number or not.
+
+    Args:
+        num (int): Number to check.
+    Returns:
+        bool: True if num is prime number, False otherwise.
+    """
+    if not isinstance(num, int):
+        raise TypeError("'num' must be an integer.")
+
     if num < 2:
         res = False
     else:
         res = True
+        for n in range(2, floor(sqrt(num) + 1)):
+            if num % n == 0:
+                res = False
+                break
 
     return res
--- test_primes.py
+++ test_primes.py
@@ -2,6 +2,27 @@
 from primes import is_prime
 
 def test_prime_low_number() -> None:
-    """tests to create function is_prime().
+    """test for lowest numbers.
     """
     assert is_prime(1) == False
+
+def test_prime_prime_number() -> None:
+    """test for a known prime number."""
+    assert is_prime(29)
+
+def test_prime_composite_number() -> None:
+    """test for a known composite number."""
+    assert is_prime(15) == False
+
+def test_prime_negative_number() -> None:
+    """test for negative number."""
+    assert is_prime(-7) == False
+
+def test_prime_non_integer() -> None:
+    """test for non-integer input."""
+    try:
+        is_prime(3.5)
+    except TypeError as te:
+        assert str(te) == "num must be an integer."
+    else:
+        assert False, "TypeError was not raised."
