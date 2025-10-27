ac25dacee81cb1e182d05c2cc66679236d2438b8
Kobaya65 <kobaya65@free.fr>
2025-10-27 12:31:26 +0100

    Docstring for test_prime_low_number()

--- test_primes.py
+++ test_primes.py
@@ -1,4 +1,6 @@
 """https://stackabuse.com/test-driven-development-with-pytest/"""
 
 def test_prime_low_number():
+    """tests to create function is_prime().
+    """
     assert is_prime(1) == False
